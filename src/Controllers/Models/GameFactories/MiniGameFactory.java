d5071d28aef1d7c4c521239d9fb8ca2f8ae87222
karolyi15 <karolyi15@hotmail.com>
2020-06-15 16:00:45 -0700

    MiniGame Factory Update

--- src/Controllers/Models/GameFactories/MiniGameFactory.java
+++ src/Controllers/Models/GameFactories/MiniGameFactory.java
@@ -1,14 +1,28 @@
 package Controllers.Models.GameFactories;
 
+import Controllers.Models.SpriteFactories.SpriteFactory;
 import Controllers.Views.Scene_Controller;
 
 public class MiniGameFactory implements iGameFactory{
 
+    //Factories
+    private SpriteFactory spriteFactory;
 
+    //Setters and Getters
+    public void setSpriteFactory(SpriteFactory spriteFactory){
+        this.spriteFactory=spriteFactory;
+    }
 
-
+    //Creation System
     @Override
     public iMiniGame createGame(Scene_Controller sceneController){
-        return  null;
+        if(this.spriteFactory!=null) {
+
+            iMiniGame tempGame = new BoardGame(sceneController, this.spriteFactory);
+            return tempGame;
+
+        }else{
+            return null;
+        }
     }
 }
